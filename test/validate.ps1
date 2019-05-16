f92cd67de3564d0082807c6b8753277f80736167
Bernard <bernard@ducourier.com>
2019-05-16 12:12:50 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -70,9 +70,9 @@ if (-not $doNotCleanup) {
 # Start the deployment
 Write-Host "Starting $templateLibraryName dependancies deployment...";
 
-New-AzureRmDeployment -Location $Location -Name "Deploy-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
+New-AzureRmDeployment -Location $Location -Name "Deploy-$templateLibraryName-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -baseParametersURL $baseParametersURL -Verbose;
 
-$provisionningState = (Get-AzureRmDeployment -Name "Deploy-Infrastructure-Dependancies").ProvisioningState
+$provisionningState = (Get-AzureRmDeployment -Name "Deploy-$templateLibraryName-Infrastructure-Dependancies").ProvisioningState
 
 if ($provisionningState -eq "Failed") {
     Write-Host "One of the jobs was not successfully created... exiting..."
